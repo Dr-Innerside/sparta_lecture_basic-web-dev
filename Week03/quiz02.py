c70ec64e494155cbca236baee1c7830a9f967af8
migdracios <migdracios@gmail.com>
2022-04-14 16:48:29 +0900

    fix Week 03 python file [quiz02.py]

--- Week03/quiz02.py
+++ Week03/quiz02.py
@@ -1,5 +1,10 @@
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.preboot
 
 # 코딩 시작
+
+movie = db.movies.find_one({'title':'매트릭스'},{'_id':False})
+print(movie['star'])
+
+db.movies.update_one({'title':'매트릭스'},{'$set':{'star':0}})
