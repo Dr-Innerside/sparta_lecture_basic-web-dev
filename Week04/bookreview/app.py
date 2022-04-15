f7d7a093c0b2a6f8ce4401c76f10636c4a3d602f
migdracios <migdracios@gmail.com>
2022-04-16 00:09:30 +0900

    add Week04 bookreview python code <bookreview db insert API> [app.py]

--- Week04/bookreview/app.py
+++ Week04/bookreview/app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.preboot
+
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 ## API 역할을 하는 부분
 @app.route('/review', methods=['POST'])
 def write_review():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 POST!'})
+    # 1. 클라이언트가 준 title, author, review 가져오기.
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+    # 2. DB에 정보 삽입하기
+    doc = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+    db.bookreview.insert_one(doc)
+    # 3. 성공 여부 & 성공 메시지 반환하기
+    return jsonify({'result': 'success', 'msg': '리뷰가 성공적으로 작성되었습니다.'})
 
 
 @app.route('/review', methods=['GET'])
