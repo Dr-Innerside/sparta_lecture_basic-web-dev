dc9f04e99de049f840119df67c96e9998bb495e6
migdracios <migdracios@gmail.com>
2022-04-17 17:14:19 +0900

    4주차 숙제 POST API 클라이언트 작성

--- Week04/homework/app.py
+++ Week04/homework/app.py
@@ -5,21 +5,37 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route('/mypage')
-def mypage():
-   return 'This is Mypage!'
 
-@app.route('/test', methods=['GET'])
-def test_get():
+# API
+#
+#  주문하기 POST : 정보 입력 후 '주문하기' 버튼 클릭 시 주문 목록에 추가
+#  주문내역보기 GET : 페이지 로딩 후 하단 주문 목록이 자동으로 보이기
+
+# POST API
+@app.route('/list', methods=['POST'])
+def list_post():
+   # POST API 설계
+   #
+   #  사용자 요청
+   #  사용자가 입력한 값을 받아옴(성함, 수량, 주소, 전화번호)
+   #  받아온 값을 DB에 삽입
+   #  데이터 응답
+   #  'msg' : '주문목록에 추가되었습니다.'
+
+   name_receive = request.form['name_give']
+   count_receive = request.form['count_give']
+   address_receive = request.form['address_give']
+   number_receive = request.form['number_give']
+   print(name_receive, count_receive, address_receive, number_receive)
+
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+# GET API
+@app.route('/list', methods=['GET'])
+def list_get():
    title_receive = request.args.get('title_give')
    print(title_receive)
    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
-
-@app.route('/test', methods=['POST'])
-def test_post():
-   title_receive = request.form['title_give']
-   print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
